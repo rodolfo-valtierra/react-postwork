e55c653c9c6995e1a397dd6f5d0e4795885938f1
rodolfo-valiterra <rodolfoisaac.valtierradominguez@gmail.com>
2022-10-10 07:51:03 -0500

    terminando use http

--- src/Components/Meal/Meals.js
+++ src/Components/Meal/Meals.js
@@ -1,31 +1,10 @@
-import { useState, useReducer } from 'react'
+import { useState, useReducer, useEffect } from 'react'
 import MealItem from "./MealItem";
 import Cart from '../Cart'
 import mealStyle from '../../styles/meals.module.css'
 import Modal from '../UI/modal'
 import CartContext from '../../Context/CartContext'
-import _ from 'lodash';
-
-const meals = [
-  {
-    id: 1,
-    platillo: 'Jamba Juice',
-    description: 'Fruit & Veggie, Orange Carrot Karma Smoothie',
-    precio: 10
-  },
-  {
-    id: 2,
-    platillo: "Florida's Natural",
-    description: '100% Orange Juice with Calcium & Vitamin D',
-    precio: 50
-  },
-  {
-    id: 3,
-    platillo: 'Spaguety',
-    description: 'pasta italiana adornada con finas hierbas',
-    precio: 120
-  }
-]
+import useHttp from '../../hooks/use-http';
 
 function ReducerCart (oldState,  ACTION) {
   switch (ACTION.type) {
@@ -41,9 +20,10 @@ function ReducerCart (oldState,  ACTION) {
 
       return {...oldState};
     };
-    case 'CHANGE': {
+    case 'CHANGE_AMOUNT': {
       const index = oldState.cart.findIndex( el => el.id==ACTION.meal.id);
-      oldState.cart[index] = ACTION.meal;
+      oldState.cart[index].cantidad += ACTION.add.cantidad;
+
 
       return {...oldState}
     };
@@ -51,41 +31,58 @@ function ReducerCart (oldState,  ACTION) {
       oldState = oldState.cart.filter(el => el.id=ACTION.dropId);
       return {...oldState};
     };
-    default: return {...oldState};
+    case 'CARTMODAL':{
+      return {...oldState, cartIsOpen: !oldState.cartIsOpen}     
+    }
+    default: return oldState;
   }
 }
 
-function Meals () {
-  const [isModalOpen, setModalOpen] = useState(false)
-  const [cartState, cartDispatcher] = useReducer(ReducerCart, {cartIsOpen: false, cart: []});
+const BASE_URL = 'https://meal-react-20cb2-default-rtdb.firebaseio.com/';
 
-  const openModal = () => setModalOpen(!isModalOpen)
+function Meals () {
+  const [cartState, cartDispatcher] = useReducer(ReducerCart, {cartIsOpen: false, cart: []});
+  const [meals, setMeals] = useState([]);
+  const { isLoading, error, request } = useHttp();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const url = `${BASE_URL}/meals.json?orderBy="$key"&isNot="0"`;
+      const data = await request({url});
+  
+      setMeals(data.filter((el, i) => i!=0))    
+    }
+
+    fetchMeals();
+  }, [])
 
   const totalProducts = () => cartState.cart.reduce((acc, el) => acc+el.precio, 0);
+
   const countProducts = () => cartState.cart.reduce((acc, el) => acc+el.cantidad, 0);
 
-  const addNewMeal = (newMeal) => {
-    cartDispatcher({type: 'ADD', meal: newMeal})
-  }
+  const addNewMeal = (newMeal) => cartDispatcher({type: 'ADD', meal: newMeal})
 
-  return <CartContext.Provider value={{productos: [...cartState.cart], count: countProducts(), total: totalProducts()}}>
-    <Cart open={openModal}/>
+  const openCartModal = () => cartDispatcher({type: 'CARTMODAL'})
+
+  return <CartContext.Provider value={{productos: cartState.cart, count: countProducts(), total: totalProducts()}}>
+    <Cart open={openCartModal}/>
 
     <div className={`${mealStyle.container}`}>
       <ul className={`${mealStyle.meal}`}>
       {
-        meals.map( meal => <MealItem
-            key={meal.id}
+        meals.length?
+        meals.map( (meal, i) => <MealItem
+            key={i}
             add={addNewMeal}
             meal={meal}
           />
-        )
+        ): <h3>there isn't meals</h3>
       }
       </ul>
     </div>
     {
-      isModalOpen?
-        <Modal onConfirm={openModal} add={cartDispatcher}/>
+      cartState.cartIsOpen?
+        <Modal onConfirm={openCartModal} add={cartDispatcher} />
         : null
     }
   </CartContext.Provider>
